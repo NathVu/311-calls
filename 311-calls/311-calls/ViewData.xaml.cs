4da78bb253e7a1e57b0e9c731cf3b40e0890fbd3
NathVu <31414875+NathVu@users.noreply.github.com>
2019-05-14 21:10:12 -0400

    Minor Refactoring
    Minor Refactoring for easier readability

--- 311-calls/311-calls/ViewData.xaml.cs
+++ 311-calls/311-calls/ViewData.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using Npgsql;
 using JsonUserVariable;
 using PgsqlDriver;
+using RowManager;
 
 namespace Group7
 {
@@ -27,8 +28,12 @@ namespace Group7
         {
             InitializeComponent();
             RowNumbers rows = new RowNumbers();
-            this.getData(rows);
+            List<Json311> data = this.getData(rows);
             OverallData.DataContext = rows;
+            DBDataBinding.ItemsSource = data;
+            Total.Text = rows.total.ToString();
+            Rows_min.Text = rows.Curr_min.ToString();
+            Rows_max.Text = rows.Curr_max.ToString();
         }
 
 
@@ -37,7 +42,8 @@ namespace Group7
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void getData(RowNumbers rows)
+        /// <returns>Returns the data to populate the table</returns>
+        private List<Json311> getData(RowNumbers rows)
         {
             Credentials creds = new Credentials();
             String user = creds.args[0];
@@ -48,13 +54,7 @@ namespace Group7
             Json311 retrieve = new Json311();
             rows.total = getConnection.GetRows(connString);
             displayData = retrieve.FillList(connString, rows.Curr_min, rows.rowsRemaining);
-            DBDataBimnding.ItemsSource = displayData;
-            Total.Text = rows.total.ToString();
-            Rows_min.Text = rows.Curr_min.ToString();
-            Rows_max.Text = rows.Curr_max.ToString();
-            rows.UpdateValuesUp();
-
-
+            return displayData;
         }
 
         /// <summary>
@@ -65,13 +65,17 @@ namespace Group7
         private void Previous_click(object sender, RoutedEventArgs e)
         {
             RowNumbers rows = ((Button)sender).DataContext as RowNumbers;
+            List<Json311> data = new List<Json311>();
             bool update = rows.UpdateValuesDown();
             if (update == true)
             {
-                getData(rows);
+                data = getData(rows);
             }
             Rows_min.Text = rows.Curr_min.ToString();
             Rows_max.Text = rows.Curr_max.ToString();
+            DBDataBinding.DataContext = rows;
+            //System.Windows.Application.LoadComponent(ViewData.xaml);
+            //DBDataBinding.GetBindingExpression(ListView.DataContextProperty).UpdateSource();
 
         }
 
@@ -82,117 +86,21 @@ namespace Group7
         /// <param name="e"></param>
         private void Next_click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Clicked");
             RowNumbers rows = ((Button)sender).DataContext as RowNumbers;
             bool update = rows.UpdateValuesUp();
+            List<Json311> data = new List<Json311>();
             if (update == true)
             {
-                getData(rows);
+                data = getData(rows);
             }
             Rows_min.Text = rows.Curr_min.ToString();
             Rows_max.Text = rows.Curr_max.ToString();
+            OverallData.DataContext = rows;
+            DBDataBinding.DataContext = data;
+            DBDataBinding.GetBindingExpression(ListView.DataContextProperty).UpdateSource();
+
         }
 
 
-    }
-
-    /// <summary>
-    /// To keep track of the records, and display the total number and the records you are currently looking through 
-    /// </summary>
-    public class RowNumbers
-    {
-
-        public int total;
-        public int Curr_min = 0;
-        public int Curr_max = 500;
-        public int rowsRemaining = 500;
-
-        /// <summary>
-        /// Updates the values if the next button is clicked
-        /// </summary>
-        /// <returns>a bool whether or not to update the values</returns>
-        public bool UpdateValuesUp()
-        {
-            if ((this.Curr_max + 500) > this.total && (this.Curr_min + 500) > this.total)
-            {
-                MessageBoxResult result = MessageBox.Show("Sorry, you cannot go any further, would you like to go back to the beginning", "Out of Bounds", MessageBoxButton.OKCancel);
-                return CarryOutUpResult(result);
-            }
-            else if ((this.Curr_max + 500) > this.total)
-            {
-                int remaining = this.total % 500;
-                this.Curr_max = total;
-                this.rowsRemaining = remaining;
-                return true;
-            }
-            else
-            {
-                this.Curr_min += 500;
-                this.Curr_max += 500;
-                return true;
-            }
-        }
-
-        /// <summary>
-        /// If it overflows this function will go back to the beginning of the table
-        /// or stay on the page if the user does not want to go back to row 0
-        /// </summary>
-        /// <param name="result">the result of the users press</param>
-        /// <returns>a bool that tells the calling function whether to update or not</returns>
-        public bool CarryOutUpResult(MessageBoxResult result)
-        {
-            switch (result)
-            {
-                case MessageBoxResult.OK:
-                    {
-                        this.Curr_min = 0;
-                        this.Curr_max = 500;
-                        this.rowsRemaining = 500;
-                        return true;
-                    }
-
-                case MessageBoxResult.Cancel:
-                    break;
-            }
-            return false;
-        }
-
-        /// <summary>
-        /// Updates the values if the Previous button is clicked
-        /// </summary>
-        /// <returns>A bool whether or not to update the values</returns>
-        public bool UpdateValuesDown()
-        {
-            if ((this.Curr_min - 500) < 500)
-            {
-                MessageBox.Show("Sorry, you cannot go any further back, would you like to go the end of the dataset?", "Our of Bounds", MessageBoxButton.OKCancel);
-                return false;
-            }
-            else
-            {
-                this.Curr_min -= 500;
-                this.Curr_max -= 500;
-                return true;
-            }
-        }
-
-        public bool CarryOutDownResult(MessageBoxResult result)
-        {
-            switch (result)
-            {
-                case MessageBoxResult.OK:
-                    {
-                        int remaining = total % 500;
-                        this.rowsRemaining = remaining;
-                        this.Curr_max = this.total;
-                        this.Curr_min = this.total - 500;
-                        return true;
-                    }
-
-                case MessageBoxResult.Cancel:
-                    break;
-            }
-            return false;
-        }
     }
 }
